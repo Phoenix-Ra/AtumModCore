b23bc8b0ae87a369f10d562a1e5360d52d2a5c9a
fenix <bogdan_t_1102@mail.ru>
2023-12-27 02:22:26 +0100

    version 1.1.1

--- atumod-api/src/main/java/me/phoenixra/atumodcore/api/AtumModProperties.java
+++ atumod-api/src/main/java/me/phoenixra/atumodcore/api/AtumModProperties.java
@@ -1,7 +1,7 @@
 package me.phoenixra.atumodcore.api;
 
             public class AtumModProperties {
-                public static final String VERSION = "1.0.0";
+                public static final String VERSION = "1.1.1";
                 public static final String MOD_NAME = "AtumModCore";
                 public static final String MOD_ID = "atumodcore";
             }
