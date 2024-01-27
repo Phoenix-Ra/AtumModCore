0c5074633258c7d32be4a57fdcc7fa23ecb50a1b
fenix <bogdan_t_1102@mail.ru>
2024-01-27 21:53:11 +0100

    javadocs: added for AtumModService

--- atumod-api/src/main/java/me/phoenixra/atumodcore/api/service/AtumModService.java
+++ atumod-api/src/main/java/me/phoenixra/atumodcore/api/service/AtumModService.java
@@ -3,9 +3,32 @@ package me.phoenixra.atumodcore.api.service;
 import net.minecraftforge.fml.common.event.FMLEvent;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A mod service is the feature allowing you to
+ * use the FMLEvents outside the AtumMod class.
+ * and control the lifecycle of the service.
+ */
 public interface AtumModService {
 
+    /**
+     * Called on FMLEvent called.
+     * Make sure that you used the notifyModServices
+     * method in your AtumMod class.
+     * Otherwise, this method will not be called.
+     *
+     * @param event The event.
+     */
     void handleFmlEvent(@NotNull FMLEvent event);
+
+    /**
+     * Called when the service is removed.
+     *
+     */
     void onRemove();
+
+    /**
+     * Get the id of the service.
+     *
+     */
     @NotNull String getId();
 }
