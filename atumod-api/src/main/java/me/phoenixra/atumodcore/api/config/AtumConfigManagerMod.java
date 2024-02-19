835a599f3d94ab1b91006204fce99b55c867dfdc
fenix <bogdan_t_1102@mail.ru>
2024-02-19 05:24:29 +0100

    fix: atumModService of configManager not registered

--- atumod-api/src/main/java/me/phoenixra/atumodcore/api/config/AtumConfigManagerMod.java
+++ atumod-api/src/main/java/me/phoenixra/atumodcore/api/config/AtumConfigManagerMod.java
@@ -2,14 +2,16 @@ package me.phoenixra.atumodcore.api.config;
 
 import me.phoenixra.atumconfig.api.ConfigOwner;
 import me.phoenixra.atumconfig.core.config.AtumConfigManager;
+import me.phoenixra.atumodcore.api.AtumMod;
 import me.phoenixra.atumodcore.api.service.AtumModService;
 import net.minecraftforge.fml.common.event.FMLEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class AtumConfigManagerMod extends AtumConfigManager implements AtumModService {
-    public AtumConfigManagerMod(ConfigOwner configOwner) {
-        super(configOwner);
+    public AtumConfigManagerMod(AtumMod atumMod) {
+        super(atumMod);
+        atumMod.provideModService(this);
     }
 
     @Override
