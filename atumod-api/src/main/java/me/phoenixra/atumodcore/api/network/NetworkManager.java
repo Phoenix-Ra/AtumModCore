ca9fa3856af8a09636aa34ca0df0c36cd834515f
fenix <bogdan_t_1102@mail.ru>
2024-03-07 18:11:05 +0100

    fix: network client-to-server not sending packets

--- atumod-api/src/main/java/me/phoenixra/atumodcore/api/network/NetworkManager.java
+++ atumod-api/src/main/java/me/phoenixra/atumodcore/api/network/NetworkManager.java
@@ -267,10 +267,6 @@ public abstract class NetworkManager {
      */
     @SideOnly(Side.CLIENT)
     public final void sendToServer(IMessage message) {
-        if(Minecraft.getMinecraft().playerController==null ||
-                Minecraft.getMinecraft().isSingleplayer()) {
-            return;
-        }
         System.out.println("Sending to server: " + message.getClass().getSimpleName());
         NETWORK_CHANNEL.sendToServer(message);
     }
